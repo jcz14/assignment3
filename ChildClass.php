57846cd237701c4bf87c8ab5bbb8c12ba6e585b2
Jim Zheng <jcz14@pitt.edu>
2016-11-03 16:08:49 -0700

    __toString not implemented yet

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,88 @@
 <?php 
-	// this file will extend PArentClass.php
+	// extends Car class
 
-	class ChildClass extends ParentClass {
-		
-	}
+	class Car extends CarBasics {
+		// private variables
+    private $doors;
+    private $transmission;
+    private $awd;
+    private $sunroof;
+    private $bluetooth;
+    private $seatwarmer;
+    private $seatmaterial;
+
+    // constructor
+    public function __construct($make, $model, $color, $mileage, $fuel, $doors, $transmission, $awd, $sunroof, $bluetooth, $seatwarmer, $seatmaterial) {
+      parent::__construct($make, $model, $color, $mileage, $fuel);
+      $this->doors = $doors;
+      $this->transmission = $transmission;
+      $this->awd = $awd;
+      $this->sunroof = $sunroof;
+      $this->bluetooth = $bluetooth;
+      $this->seatwarmer = $seatwarmer;
+      $this->seatmaterial = $seatmaterial;
+    }
+
+    // returns the features of this car
+    public function __toString() {
+      // still need to implement this
+    }
+
+    // getters
+    public function getDoors() {
+      return $this->doors;
+    }
+
+    public function getTransmission() {
+      return $this->transmission;
+    }
+
+    public function getAwd() {
+      return $this->awd;
+    }
+
+    public function getSunroof() {
+      return $this->sunroof;
+    }
+
+    public function getBluetooth() {
+      return $this->bluetooth;
+    }
+
+    public function getSeatwarmer() {
+      return $this->seatwarmer;
+    }
+
+    public function getSeatmaterial() {
+      return $this->seatmaterial;
+    }
+
+    // setters
+    public function setDoors($doors) {
+      $this->doors = $doors;
+    }
+
+    public function setTransmission($transmission) {
+      $this->transmission = $transmission;
+    }
+
+    public function setAwd($awd) {
+      $this->awd = $awd;
+    }
+
+    public function setSunroof($sunroof) {
+      $this->sunroof = $sunroof;
+    }
+
+    public function setBluetooth($bluetooth) {
+      $this->bluetooth = $bluetooth;
+    }
+
+    public function setSeatwarmer($seatwarmer) {
+      $this->seatwarmer = $seatwarmer;
+    }
+
+    public function setSeatmaterial($seatmaterial) {
+      $this->seatmaterial = $seatmaterial;
+    }
+  }
